ad006a35cc9ea99ac5adb7cf3d3c407e49469613
kguth <karoly.guth@capgemini.com>
2023-10-05 13:59:09 +0200

    Theming

---
+++ app/src/main/java/com/example/composepractice/screen/theming/MovieCard.kt
@@ -0,0 +1,54 @@
+package com.example.composepractice.screen.theming
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MovieCard(movie: Movie, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        )
+    ){
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ){
+                Image(painter = painterResource(
+                    id = movie.imageResourceId),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp)
+                        .clip(MaterialTheme.shapes.medium),
+                    contentScale = ContentScale.Crop)
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = movie.title,
+                    color = MaterialTheme.colorScheme.onPrimary)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "Director: ${movie.director}")
+                Text(text = "Director: ${movie.releaseYear}")
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "Director: ${movie.description}")
+            }
+    }
+
+}
+
